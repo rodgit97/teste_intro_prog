fba257ef5a8dd0c9e90c6f5ede3eeb3bb48e25e4
Rodrigo <2148722@student.uma.pt>
2026-09-15 19:41:17 +0100

    exemp1
    criar 1 exemplo

---
+++ .idea/exemp1.py
@@ -0,0 +1,13 @@
+comida = "pizza"
+preco = 10.99
+num_comida = int (input(f'quanta comida tem {comida}? '))
+total = num_comida * preco
+
+print(f'total a pagar {total}')
+
+print("----------------//-----------------")
+nome_inteiro = " bro code"
+age= 29
+print(f'{nome_inteiro} tem {age} anos')
+print(f'{nome_inteiro} ')
+print(f'tem {age} anos')
