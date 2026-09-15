75318c3be8c15c78aa32f6a2a8c86b7f5577cc34
Rodrigo <2148722@student.uma.pt>
2026-09-15 22:42:37 +0100

    exemplo1-2
    mais

--- .idea/exemp1.py
+++ .idea/exemp1.py
@@ -6,8 +6,66 @@ total = num_comida * preco
 print(f'total a pagar {total}')
 
 print("----------------//-----------------")
-nome_inteiro = " bro code"
+nome_inteiro = " rodrigo"
 age= 29
 print(f'{nome_inteiro} tem {age} anos')
 print(f'{nome_inteiro} ')
 print(f'tem {age} anos')
+print("----------------//-----------------")
+print("gosto de asranhas")
+
+print("----------------//-----------------")
+#strings
+
+primeiro_nome = "rodrick"
+comer= "pizza"
+email="rodrick26@falso.pt"
+
+print(primeiro_nome)
+print(f'bom dia, {primeiro_nome}')
+print(f'voce gosta de {comer}')
+print(f'seu email: {email}')
+
+print("----------------//-----------------")
+#intengers
+idade = "28"
+quantidade = 3
+num_alunos = 12
+print(f'{idade} anos')
+print(f'tu compraste {quantidade} itens')
+print(f'a sua turma contem {num_alunos} alunos')
+
+print("----------------//-----------------")
+#floats
+preço = 10.99
+gpa= 3.2
+distancia = 5.5
+
+print(f'o preço é {preço}')
+print(f'o gpa é {gpa}')
+print(f'a disctancia é {distancia}')
+
+print("----------------//-----------------")
+#boolean
+is_student = True
+print(f'voce é um estudante?  {is_student}')
+
+is_student = False
+if is_student:
+    print("tu es um estudante")
+else:
+    print("não es um estudante")
+
+is_student = False
+for_sale=False
+is_online= True
+
+if for_sale:
+    print("este item é prar a venda")
+else:
+    print("este item não é prar a venda")
+
+if is_online:
+    print("está em online")
+else:
+    print("está em offline")
