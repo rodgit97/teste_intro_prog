57eede2d0c4e35c43982b7f117d10ccebda5ff18
Rodrigo <2148722@student.uma.pt>
2026-09-15 23:16:40 +0100

    exemplo2
    exemplo outro 2

---
+++ .idea/exemp2.py
@@ -0,0 +1,32 @@
+# str int float bool
+nome ="rodrigo"
+idade=20
+gpa=3.2
+e_estudante = True
+
+print(type(nome))
+print(type(e_estudante))
+
+gpa = int(gpa)
+print(gpa)
+
+idade = float(idade)
+print(idade)
+
+idade = str(idade)
+print(type(idade))
+
+print("-------------//---------------")
+idade = str(idade)
+idade += "1"
+print(idade)
+
+
+print("-------------//---------------")
+nome = " "
+nome = bool(nome)
+print(nome)
+print("-------------//---------------")
+
+
+
