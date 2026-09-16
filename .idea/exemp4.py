a7ae72a5a314ad01660977e78667d91136f67e12
Rodrigo <2148722@student.uma.pt>
2026-09-16 23:51:44 +0100

    exemplo4
    4

---
+++ .idea/exemp4.py
@@ -0,0 +1,12 @@
+#adjetivo1 ="penis"
+adjetivo1 = input("dá um adjetivo:")
+noun1 = input("dá um nome:")
+adjetivo2 = input("dá um adjetivo:")
+verbo1 = input("dá um verbo:")
+adjetivo3 = input("dá um adjetivo:")
+
+print(f"hoje eu fui ao {adjetivo1} zoologico" )
+print(f"na exibicao eu vi  {noun1} " )
+print(f"{noun1} estava {adjetivo2}  e {verbo1}" )
+print(f"eu estava {adjetivo3} !" )
+
