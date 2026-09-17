9da091653edf00d5d5ff105d312b00e016505240
Rodrigo <2148722@student.uma.pt>
2026-09-18 00:14:48 +0100

    exemplo11
    11

---
+++ .idea/exemp11.py
@@ -0,0 +1,40 @@
+num = -5
+print("posivito" if num > 0 else "negativo")
+
+print("--//--")
+num= 6
+resultado= "EVEN" if num%2==0 else "ODD"
+print(resultado)
+
+print("--//--")
+a=6
+b=7
+max_num = a if a>b else b
+min_num = a if a<b else b
+
+print(max_num)
+print(min_num)
+
+print("--//--")
+a=6
+b=7
+idade = 25
+
+status=  "Adult0" if idade>=18 else "criança"
+print(status)
+
+print("--//--")
+
+temperatura= 30
+
+clima="QUENTE   " if temperatura> 20 else "FRIO"
+print(clima)
+
+print("--//--")
+
+lote_utilizador = "admin"
+nivel_acesso =  "Acesso Autorizado" if lote_utilizador =="admin" else "Acesso Limitado"
+
+print(nivel_acesso)
+
+print("--//--")
