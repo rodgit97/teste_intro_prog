e8765ab10f183c1f1deea16487228d021aef1eb1
Rodrigo <2148722@student.uma.pt>
2026-09-18 23:57:18 +0100

    exemplo13
    13

---
+++ .idea/exemp13.py
@@ -0,0 +1,19 @@
+numero_credito = "1234-5678-9012-3456"
+print(numero_credito[0])
+print(numero_credito[4])
+print(numero_credito[1])
+print(numero_credito[0:4])
+print(numero_credito[:6])
+print(numero_credito[7:])
+print(numero_credito[7:12])
+print(numero_credito[-1])
+print(numero_credito[-3])
+print(numero_credito[-7])
+print(numero_credito[::2])
+print(numero_credito[::3])
+print(numero_credito[::3])
+
+ultimos_digitos = numero_credito[-4:]
+print(f"xxxx-xxxx-xxxx-{ultimos_digitos}")
+print(numero_credito)
+
