d12f211e5bf6525ed77b9284d92606b2d4210313
Rodrigo <2148722@student.uma.pt>
2026-09-19 22:17:42 +0100

    exemplo14
    14

---
+++ .idea/exemp14.py
@@ -0,0 +1,69 @@
+#loops
+"""
+loopsz
+"""
+preco1=3.21159
+preco2=-987.65
+preco3=12.34
+
+print(f"preco 1 is {preco1}")
+print(f"preco 2 is {preco2}")
+print(f"preco 3 is {preco3}")
+print("-----------------------")
+print(f"preco 1 is {preco1:.2f}")
+print(f"preco 2 is {preco2:.2f}")
+print(f"preco 3 is {preco3:.2f}")
+print("-----------------------")
+print(f"preco 1 is {preco1:.1f}")
+print(f"preco 2 is {preco2:.1f}")
+print(f"preco 3 is {preco3:.1f}")
+
+print("-----------------------")
+print(f"preco 1 is {preco1:.3f}")
+print(f"preco 2 is {preco2:.3f}")
+print(f"preco 3 is {preco3:.3f}")
+
+print("-----------------------")
+print(f"preco 1 is {preco1:10}")
+print(f"preco 2 is {preco2:10}")
+print(f"preco 3 is {preco3:10}")
+
+print("-----------------------")
+print(f"preco 1 is {preco1:010}")
+print(f"preco 2 is {preco2:010}")
+print(f"preco 3 is {preco3:010}")
+
+print("-----------------------")
+print(f"preco 1 is {preco1:<10}")
+print(f"preco 1 is {preco2:<10}")
+print(f"preco 1 is {preco3:<10}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:>10}")
+print(f"preco 1 is {preco2:>10}")
+print(f"preco 1 is {preco3:>10}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:^10}")
+print(f"preco 1 is {preco2:^10}")
+print(f"preco 1 is {preco3:^10}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:+}")
+print(f"preco 1 is {preco2:+}")
+print(f"preco 1 is {preco3:+}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:,}")
+print(f"preco 1 is {preco2:,}")
+print(f"preco 1 is {preco3:,}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:,.2f}")
+print(f"preco 1 is {preco2:,.2f}")
+print(f"preco 1 is {preco3:,.2f}")
+
+print("-----------------------")
+print(f"preco 1 is ${preco1:+,.2f}")
+print(f"preco 1 is {preco2:+,.2f}")
+print(f"preco 1 is {preco3:+,.2f}")
