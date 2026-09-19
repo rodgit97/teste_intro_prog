3641d91a64c6f7bc50a4f2f2c6937484304bb2fd
Rodrigo <2148722@student.uma.pt>
2026-09-19 23:54:17 +0100

    exemplo17
    17

---
+++ .idea/exemp17.py
@@ -0,0 +1,44 @@
+#loops for
+for x in range(1, 11):
+    print(x)
+
+print("---------------")
+
+for x in range(1, 11):
+    print(x)
+print("coisa")
+print("---------------")
+
+for x in reversed(range(1, 11)):
+    print(x)
+
+print("---------------")
+for x in range(1, 11,2):
+    print(x)
+#impar
+print("---------------")
+for x in range(1, 11,3):
+    print(x)
+#impar
+print("---------------")
+credito_cartao="1234-5678-9012-3456"
+for x in credito_cartao:
+    print(x)
+#impar
+print("---------------")
+
+print("---------------")
+for y in range(1, 21):
+    if y == 13:
+        continue
+    else:
+        print(y)
+print("---------------")
+
+for y in range(1, 21):
+    if y == 13:
+        break
+    else:
+        print(y)
+print("---------------")
+
